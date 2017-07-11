da4939b24a356fa91aca563cc13d5b670aa43b0e
derek smith <dereksmith2112@verizon.net>
2017-07-11 10:51:15 -0500

    new look

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ app.controller("featuredController", function($scope){
   $scope.items = [
     {name: "spinner", price:3, image: "spinner.jpeg"},
     {name:"knife", price:20, image: "knife.jpg"},
-    {name:"wallet", price: 35, image: null}
+    {name:"wallet", price: 35, image: "wallet.jpg"}
   ]
 });
