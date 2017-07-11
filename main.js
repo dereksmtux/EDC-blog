bc6fd79f9316663fcc2efe79f01f2b836e40ec48
dereksmtux <dereksmith2112@verizon.net>
2017-07-10 23:37:32 -0500

    added angular

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = angular.module("featured", []);
+
+app.controller("featuredController", function($scope){
+  $scope.items = [
+    {name: "spinner", price:3, image: "spinner.jpeg"},
+    {name:"knife", price:20, image: "knife.jpg"},
+    {name:"wallet", price: 35, image: null}
+  ]
+});
