dde183d2d98b964a3140b36d52c9adfb498343c6
dereksmtux <dereksmith2112@verizon.net>
2017-07-14 00:22:21 -0500

    store comming together

--- main.js
+++ main.js
@@ -17,9 +17,13 @@ var app = angular.module("store", []);
 app.controller("storeController", function($scope){
   //console.log("hi");
   $scope.storeItems = [
-    {name: "Swiss Army Knife", price: 10, image: "swissArmyKnife.jpg", description: "Enjoy the simplicity and utility of the victorinox cadet, this styish multi-tool is perfect for urban edc"},
-    {name: "Spinner", price: 3, image: "spinner.jpeg", description: "it spins."}
+    {name: "Swiss Army Knife", price: 10, image: "swissArmyKnife.jpg", description: "Enjoy the simplicity and utility of the victorinox cadet, this styish multi-tool is perfect for urban edc."},
+    {name: "Spinner", price: 3, image: "spinner.jpeg", description: "it spins."},
+    {name:"wallet", price: 35, image: "wallet.jpg", description: "holds money."},
+    {name:"knife", price:20, image: "knife.jpg", description: "regular knife."}
   ]
+
+
 
 });
 
