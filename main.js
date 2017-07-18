e97436fb4136fd3c58e72ce18a31b0f071e5108d
derek smith <dereksmith2112@verizon.net>
2017-07-18 18:07:59 -0500

    added new structure and reviews but still needs much work

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var app = angular.module("featured", []);
 app.controller("featuredController", function($scope){
 
   $scope.items = [
-    {name: "spinner", price:3, image: "spinner.jpeg"},
-    {name:"knife", price:20, image: "knife.jpg"},
-    {name:"wallet", price: 35, image: "wallet.jpg"}
+    {name: "spinner", price:3, image: "images/spinner.jpeg"},
+    {name:"knife", price:20, image: "images/knife.jpg"},
+    {name:"wallet", price: 35, image: "images/wallet.jpg"}
   ]
 
 
@@ -17,10 +17,10 @@ var app = angular.module("store", []);
 app.controller("storeController", function($scope){
   //console.log("hi");
   $scope.storeItems = [
-    {name: "Swiss Army Knife", price: 10, image: "swissArmyKnife.jpg", description: "Enjoy the simplicity and utility of the victorinox cadet, this styish multi-tool is perfect for urban edc."},
-    {name: "Spinner", price: 3, image: "spinner.jpeg", description: "it spins."},
-    {name:"wallet", price: 35, image: "wallet.jpg", description: "holds money."},
-    {name:"knife", price:20, image: "knife.jpg", description: "regular knife."}
+    {name: "Swiss Army Knife", price: 10, image: "images/swissArmyKnife.jpg", description: "Enjoy the simplicity and utility of the victorinox cadet, this styish multi-tool is perfect for urban edc."},
+    {name: "Spinner", price: 3, image: "images/spinner.jpeg", description: "it spins."},
+    {name:"wallet", price: 35, image: "images/wallet.jpg", description: "holds money."},
+    {name:"knife", price:20, image: "images/knife.jpg", description: "regular knife."}
   ]
 });
 
--- reviews.js
+++ reviews.js
@@ -2,7 +2,8 @@ var app = angular.module("reviews", []);
 
 app.controller("reviewsController", function($scope){
   $scope.reviewItems = [
-    {name: "Swiss Army Knife", price: 10, video: "https://www.youtube.com/embed/xa_nj8NfQ9Q", description: "Enjoy the simplicity and utility of the victorinox cadet, this styish multi-tool is perfect for urban edc."}
+    {name: "Swiss Army Knife", price: 10, image: "images/swissArmyKnife.jpg" , link: "reviews/swissArmyKnifeReview.html", description: "Enjoy the simplicity and utility of the victorinox cadet, this styish multi-tool is perfect for urban edc."},
+    {name:"wallet", price: 35, image: "images/wallet.jpg", description: "holds money."}
   ]
 });
 angular.bootstrap(document.getElementById("reviewPage"), ['reviews']);
