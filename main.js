0099038d4eedea81da9f4109972ce4578d1f8381
derek smith <dereksmith2112@verizon.net>
2017-07-19 14:44:27 -0500

    fixed structure

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var app = angular.module("featured", []);
 app.controller("featuredController", function($scope){
 
   $scope.items = [
-    {name: "spinner", price:3, image: "images/spinner.jpeg"},
-    {name:"knife", price:20, image: "images/knife.jpg"},
-    {name:"wallet", price: 35, image: "images/wallet.jpg"}
+    {name: "spinner", price:3, image: "spinner.jpeg"},
+    {name:"knife", price:20, image: "knife.jpg"},
+    {name:"wallet", price: 35, image: "wallet.jpg"}
   ]
 
 
